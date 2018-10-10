d5c499a3571bdd15d9d1d71fcfcb97a0694741b6
rajat <rajatgupta0889@gmail.com>
2018-10-10 11:33:46 +0530

    Issue #SB-0000 add systemsetting test cases

--- sunbird_service_api_test/src/test/java/org/sunbird/common/action/SystemSettingUtil.java
+++ sunbird_service_api_test/src/test/java/org/sunbird/common/action/SystemSettingUtil.java
@@ -3,47 +3,58 @@ package org.sunbird.common.action;
 import com.consol.citrus.context.TestContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.util.StringUtils;
 import org.sunbird.common.util.Constant;
 import org.sunbird.integration.test.common.BaseCitrusTestRunner;
 
-/**
- * Created by rajatgupta on 09/10/18.
- */
+/** Created by rajatgupta on 09/10/18. */
 public class SystemSettingUtil {
+  public static String ID = "uniqueId";
+  public static String FIELD = "uniqueField";
+  public static final String TEMPLATE_DIR = "templates/systemsetting/create";
 
-    public static final String TEMPLATE_DIR = "templates/systemsetting/create";
-    private static String getCreateSystemSettingUrl(BaseCitrusTestRunner runner) {
-        return runner.getLmsApiUriPath("/api/data/v1/system/settings/set", "/v1/system/setting/set");
+  private static String getCreateSystemSettingUrl(BaseCitrusTestRunner runner) {
+    return runner.getLmsApiUriPath("/api/data/v1/system/settings/set", "/v1/system/setting/set");
+  }
+
+  public static void createSystemSetting(
+      BaseCitrusTestRunner runner,
+      TestContext testContext,
+      String templateDir,
+      String testName,
+      String variable) {
+
+    runner.http(
+        builder ->
+            TestActionUtil.getPostRequestTestAction(
+                builder,
+                Constant.LMS_ENDPOINT,
+                templateDir,
+                testName,
+                getCreateSystemSettingUrl(runner),
+                Constant.REQUEST_JSON,
+                MediaType.APPLICATION_JSON.toString(),
+                TestActionUtil.getHeaders(true)));
+
+    runner.http(
+        builder ->
+            TestActionUtil.getExtractFromResponseTestAction(
+                testContext,
+                builder,
+                Constant.LMS_ENDPOINT,
+                HttpStatus.OK,
+                "$.result.id",
+                variable));
+    runner.sleep(Constant.ES_SYNC_WAIT_TIME);
+  }
+
+  public static String getSystemSetting(BaseCitrusTestRunner runner, TestContext testContext) {
+    runner.variable("id", ID);
+    runner.variable("field", FIELD);
+    if (StringUtils.isEmpty((String) testContext.getVariables().get("systemSetting"))) {
+      createSystemSetting(
+          runner, testContext, TEMPLATE_DIR, "testCreateSystemSettingSuccess", "systemSetting");
     }
-
-    public static void createSystemSetting(
-            BaseCitrusTestRunner runner,
-            TestContext testContext,
-            String templateDir,
-            String testName,
-            String variable) {
-
-        runner.http(
-                builder ->
-                        TestActionUtil.getPostRequestTestAction(
-                                builder,
-                                Constant.LMS_ENDPOINT,
-                                templateDir,
-                                testName,
-                                getCreateSystemSettingUrl(runner),
-                                Constant.REQUEST_JSON,
-                                MediaType.APPLICATION_JSON.toString(),
-                                TestActionUtil.getHeaders(false)));
-
-        runner.http(
-                builder ->
-                        TestActionUtil.getExtractFromResponseTestAction(
-                                testContext,
-                                builder,
-                                Constant.LMS_ENDPOINT,
-                                HttpStatus.OK,
-                                "$.result.id",
-                                variable));
-        runner.sleep(Constant.ES_SYNC_WAIT_TIME);
-    }
+    return testContext.getVariable("systemSetting");
+  }
 }
