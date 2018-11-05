4052c0fa49f5f0ad6728296f9c728dac7c60a76a
Rahul Kumar <Rahul Kumar>
2018-11-05 12:15:55 +0530

    SB0000 FIX converted the add user to org with empty role array test to be a success test cases

--- sunbird_service_api_test/src/test/java/org/sunbird/integration/test/org/AddUserToOrgTest.java
+++ sunbird_service_api_test/src/test/java/org/sunbird/integration/test/org/AddUserToOrgTest.java
@@ -4,14 +4,16 @@ import com.consol.citrus.annotations.CitrusTest;
 import com.consol.citrus.testng.CitrusParameters;
 import javax.ws.rs.core.MediaType;
 import org.springframework.http.HttpStatus;
+import org.sunbird.common.action.OrgUtil;
+import org.sunbird.common.action.UserUtil;
 import org.sunbird.integration.test.common.BaseCitrusTestRunner;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class AddUserToOrgTest extends BaseCitrusTestRunner {
 
-  public static final String TEST_ADD_USER_TO_ORG_FAILURE_WITH_EMPTY_ROLE_ARRAY =
-      "testAddUserToOrgFailureWithEmptyRoleArray";
+  public static final String TEST_ADD_USER_TO_ORG_SUCCESS_WITH_EMPTY_ROLE_ARRAY =
+      "testAddUserToOrgSuccessWithEmptyRoleArray";
 
   public static final String TEST_NAME_ADD_USER_TO_ORG_FAILURE_WITH_INVALID_USER_ID =
       "testAddUserToOrgFailureWithInvalidUserId";
@@ -28,7 +30,6 @@ public class AddUserToOrgTest extends BaseCitrusTestRunner {
   public Object[][] addUserToOrgFailureDataProvider() {
 
     return new Object[][] {
-      new Object[] {TEST_ADD_USER_TO_ORG_FAILURE_WITH_EMPTY_ROLE_ARRAY},
       new Object[] {TEST_NAME_ADD_USER_TO_ORG_FAILURE_WITH_INVALID_USER_ID},
       new Object[] {TEST_NAME_ADD_USER_TO_ORG_FAILURE_WITH_INVALID_ORG_ID},
     };
@@ -49,5 +50,35 @@ public class AddUserToOrgTest extends BaseCitrusTestRunner {
         true,
         HttpStatus.BAD_REQUEST,
         RESPONSE_JSON);
+  }
+
+  @Test()
+  @CitrusTest
+  public void testAddUserToOrgSuccessWithEmptyRoleArray() {
+    getTestCase().setName(TEST_ADD_USER_TO_ORG_SUCCESS_WITH_EMPTY_ROLE_ARRAY);
+    getAuthToken(this, true);
+    createUser();
+    createOrg();
+    performPostTest(
+        this,
+        TEMPLATE_DIR,
+        TEST_ADD_USER_TO_ORG_SUCCESS_WITH_EMPTY_ROLE_ARRAY,
+        getAddUserToOrgUrl(),
+        REQUEST_JSON,
+        MediaType.APPLICATION_JSON,
+        true,
+        HttpStatus.OK,
+        RESPONSE_JSON);
+  }
+
+  private void createUser() {
+    UserUtil.getUserId(this, testContext);
+    variable("userId", testContext.getVariable("userId"));
+  }
+
+  private void createOrg() {
+    variable("rootOrgChannel", OrgUtil.getRootOrgChannel());
+    OrgUtil.getRootOrgId(this, testContext);
+    variable("organisationId", testContext.getVariable("organisationId"));
   }
 }
