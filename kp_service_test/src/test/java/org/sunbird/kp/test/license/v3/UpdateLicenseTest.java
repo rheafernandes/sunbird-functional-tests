5b84871469dde3489f99c9976a4ff09cd6df019a
rheafernandes <rheafernandeshallu@gmail.com>
2019-11-14 15:24:22 +0530

    Fixed issue of directory in license

--- kp_service_test/src/test/java/org/sunbird/kp/test/license/v3/UpdateLicenseTest.java
+++ kp_service_test/src/test/java/org/sunbird/kp/test/license/v3/UpdateLicenseTest.java
@@ -3,8 +3,6 @@ package org.sunbird.kp.test.license.v3;
 import com.consol.citrus.annotations.CitrusTest;
 import com.consol.citrus.testng.CitrusParameters;
 import org.sunbird.kp.test.common.BaseCitrusTestRunner;
-import org.sunbird.kp.test.content.v3.ContentV3Scenario;
-import org.sunbird.kp.test.util.LicenseUtil;
 import org.sunbird.kp.test.util.TestSetupUtil;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.DataProvider;
@@ -13,24 +11,61 @@ import org.testng.annotations.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Integration Test Cases for Update License API
+ *
+ * @author rheashalomannfernandes
+ *
+ * Number of Test cases :
+ * Last Updated the Test cases on:
+ */
 public class UpdateLicenseTest extends BaseCitrusTestRunner {
     private static final String TEMPLATE_DIR = "templates/license/v3/update";
+    // This map populates the id's and directories
     private static Map<String, String> dirIdMap = new HashMap<>();
 
+    //Comment this out after populating the data
     @AfterClass
     public static void populateAssertionData() {
         TestSetupUtil.createDirectoriesForTestCases(dirIdMap, "validate.json", TEMPLATE_DIR);
     }
 
-    @Test(dataProvider = "createLicenseWithValidRequest")
+    @Test(dataProvider = "updateLicenseWithValidRequest")
     @CitrusParameters("testName")
     @CitrusTest
     public void testCreateResourceContentWithValidRequest(String testName) {
+//        String identifier = (String) LicenseUtil.createLicense(this, null, null).get("identifier");
+        dirIdMap.put(testName, "");
 
     }
 
-    @DataProvider(name = "createLicenseWithValidRequest")
+    @DataProvider(name = "updateLicenseWithValidRequest")
     public Object[][] createLicenseWithValidRequest() {
-        return null;
+        return new Object[][]{
+                new Object[]{
+                        LicenseV3Scenario.TEST_UPDATE_LICENSE_WITH_VALID_REQUEST
+                },
+                new Object[]{
+                        LicenseV3Scenario.TEST_UPDATE_LICENSE_WITH_STATUS
+                },
+                new Object[]{
+                        LicenseV3Scenario.TEST_UPDATE_LICENSE_WITH_NEW_METADATA
+                },
+                new Object[]{
+                        LicenseV3Scenario.TEST_UPDATE_LICENSE_WITH_METADATA_NAME_CASE_MISMATCH
+                },
+                new Object[]{
+                        LicenseV3Scenario.TEST_UPDATE_LICENSE_WITH_CODE
+                },
+                new Object[]{
+                        LicenseV3Scenario.TEST_UPDATE_LICENSE_WITH_DATA_TYPE_MISMATCH
+                },
+                new Object[]{
+                        LicenseV3Scenario.TEST_UPDATE_LICENSE_WITH_IDENTIFIER
+                },
+                new Object[]{
+                        LicenseV3Scenario.TEST_UPDATE_LICENSE_WITH_INVALID_STATUS
+                },
+        };
     }
 }
