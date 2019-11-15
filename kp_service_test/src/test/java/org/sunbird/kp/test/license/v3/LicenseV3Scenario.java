8e532c76a97a3d646c88f8ea575636fd18255a1f
neha0305verma <neha0305verma@gmail.com>
2019-11-15 16:38:29 +0530

    license create scenarios are added

--- kp_service_test/src/test/java/org/sunbird/kp/test/license/v3/LicenseV3Scenario.java
+++ kp_service_test/src/test/java/org/sunbird/kp/test/license/v3/LicenseV3Scenario.java
@@ -12,4 +12,16 @@ public class LicenseV3Scenario {
     public static final String TEST_UPDATE_LICENSE_WITH_METADATA_NAME_CASE_MISMATCH = "testUpdateLicenseWithMetadataNameCaseMismatch";
     public static final String TEST_UPDATE_LICENSE_WITH_DATA_TYPE_MISMATCH = "testUpdateLicenseWithDataTypeMismatch";
 
+    //Create License Api Scenarios
+    public static final String TEST_CREATE_LICENSE_WITH_VALID_REQUEST = "testCreateLicenseWithValidRequest";
+    public static final String TEST_CREATE_LICENSE_WITHOUT_NAME = "testCreateLicenseWithoutName";
+    public static final String TEST_CREATE_LICENSE_WITHOUT_CODE = "testCreateLicenseWithoutCode";
+    public static final String TEST_CREATE_LICENSE_WITHOUT_URL = "testCreateLicenseWithoutUrl";
+    public static final String TEST_CREATE_LICENSE_WITH_NEW_METADATA = "testCreateLicenseWithNewMetadata";
+    public static final String TEST_CREATE_LICENSE_WITH_IDENTIFIER = "testCreateLicenseWithIdentifier";
+    public static final String TEST_CREATE_LICENSE_WITH_STATUS = "testCreateLicenseWithStatus";
+    public static final String TEST_CREATE_LICENSE_WITH_METADATA_NAME_CASE_MISMATCH = "testCreateLicenseWithMetadataNameCaseMismatch";
+    public static final String TEST_CREATE_LICENSE_WITH_DATA_TYPE_MISMATCH = "testCreateLicenseWithDataTypeMismatch";
+    public static final String TEST_CREATE_LICENSE_WITH_DUPLICATE_IDENTIFIER = "testCreateLicenseWithDuplicateIdentifier";
+    public static final String TEST_CREATE_LICENSE_WITH_SLUGGIFIED_CODE = "testCreateLicenseWithSluggifiedCode";
 }
