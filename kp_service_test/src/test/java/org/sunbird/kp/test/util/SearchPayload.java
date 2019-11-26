ab93a5bcc14108a980458e652d65da29f16477d8
Kumar Gauraw <er.krgauraw@gmail.com>
2019-11-26 17:50:12 +0530

    Issue #SC-0000 feat: updated test cases for relatedBoards

--- kp_service_test/src/test/java/org/sunbird/kp/test/util/SearchPayload.java
+++ kp_service_test/src/test/java/org/sunbird/kp/test/util/SearchPayload.java
@@ -2,11 +2,10 @@ package org.sunbird.kp.test.util;
 
 public class SearchPayload {
 
-	public static final String SEARCH_CONTENT_WITH_IDENTIFIER_AND_BOARD= "{\n" +
+	public static final String SEARCH_CONTENT_WITH_BOARD = "{\n" +
 			"    \"request\": {\n" +
 			"        \"filters\": {\n" +
 			"            \"objectType\": \"Content\",\n" +
-			"            \"identifier\": contentIdVal,\n" +
 			"            \"board\": \"boardVal\",\n" +
 			"            \"status\": []\n" +
 			"        }\n" +
